41f540b5463d208ba74d305085ce0764c685b285
gyxqqq <2277904687@qq.com>
2024-11-27 10:57:38 +0800

    Revert "重构玩家更新逻辑，移除飞行模式和攻击检测相关代码，简化游戏状态管理"
    This reverts commit 01e911255a20ebe6e2d342f23d97103ddabef535.

--- game.hpp
+++ game.hpp
@@ -28,7 +28,10 @@ public:
     std::string savepath;
     int init(std::string name);
     int load(std::string name);
-
+    int player_on_ground(player *player);
+    float get_distance_to_ground(player *player);
+    int player_attack_side(player *player, int side);
+    float get_distance_to_side(player *player, int side);
     int update();
     int save();
 };
